dd82f655ff1affb7432d243570eb554d0eda5d30
Iris Lee <irislee4git@gmail.com>
2024-04-19 15:46:43 -0400

    added portugal city info to database.js

---
+++ scripts/database.js
@@ -0,0 +1,27 @@
+const portugalData = {
+    cities: [
+        {
+            name: 'Porto', 
+            crimeIndex: 31.78,
+            pic: 'https://images.surferseo.art/b9a896a1-5675-48e5-b666-d75c88282598.jpeg'
+        }, {
+            name: 'Lisbon', 
+            crimeIndex: 25.35, 
+            pic:'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/lisbon-2-50e7b39.jpg?quality=90&resize=556,505'
+        }, {
+            name: 'Braga',
+            crimeIndex: 42.89,
+            pic: 'https://lp-cms-production.imgix.net/2019-06/GettyImages-149519944_super%20copy.jpg'
+        }, {
+            name: 'Coimbra',
+            crimeIndex: 25.43,
+            pic: 'https://joinatlantis.com/app/uploads/2023/08/iStock-1412885232-1680x1120.jpg'
+        }
+    ], 
+    celebrities: [
+
+    ], 
+    landmarks: [
+
+    ]
+}
