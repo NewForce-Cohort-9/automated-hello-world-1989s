da309617619d0130cd38678ac024c61b79d1041f
C-Red20 <caila.linger@yahoo.com>
2024-04-22 15:48:21 -0400

    Update for Merge J

--- scripts/ireland/celebrities.js
+++ scripts/ireland/celebrities.js
@@ -1,1 +1,16 @@
 import { getIrelandcelebrities } from "../database.js";
+
+
+const celebrities = getIrelandcelebrities()
+
+export const celebIList = () => {
+    let celebIHTML = ""
+
+    for (const celeb of celebrities) {
+        celebIHTML += `${celeb.image}`
+        celebIHTML += `Name:${celeb.name}`
+        celebIHTML += `Fun Info: ${celeb.fact}`
+    }
+     
+    return celebIHTML
+} ;
